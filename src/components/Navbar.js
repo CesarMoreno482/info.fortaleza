1f68e54ed36e26083bc69e7ff149a09a80555753
Cesar Augusto <cesaraugustomoreno482@gmail.com>
2024-12-27 16:26:42 -0400

    cambios

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-        <li><a href="#" data-i18n="history_link">Historia</a></li>
-        <li><a href="#" data-i18n="features_link">Características</a></li>
-        <li><a href="#" data-i18n="gallery_link">Galería</a></li>
-        <li><a href="#" data-i18n="contact">Contacto</a></li>
+        <li><a href="#history" data-i18n="history_link">Historia</a></li>
+        <li><a href="#features" data-i18n="features_link">Características</a></li>
+        <li><a href="#gallery" data-i18n="gallery_link">Galería</a></li>
+        <li><a href="#contact" data-i18n="contact">Contacto</a></li>
       </ul>
     </nav>
   );
